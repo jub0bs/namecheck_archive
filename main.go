55d7bd56eb1b070aa63f8aa8096cb8fe3bf8baa3
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-28 12:13:25 +0100

    implement validity on GitHub in main function

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func main() {
 	username := "jub0bs"
+	if strings.HasPrefix(username, "-") ||
+		strings.HasSuffix(username, "-") ||
+		strings.Contains(username, "--") {
+		return
+	}
+	if match, _ := regexp.MatchString(`^[a-zA-Z0-9-]{3,39}$`, username); !match {
+		return
+	}
 	fmt.Println(username)
 }
