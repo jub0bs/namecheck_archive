b952cc11b05ea6b9a529da89cc3f5f73eefe785a
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-28 16:03:00 +0100

    extract validation logic to a function named IsValid

--- main.go
+++ main.go
@@ -7,14 +7,22 @@ import (
 )
 
 func main() {
-	username := "jub0bs"
+	fmt.Println(IsValid("jub0bs"))
+	fmt.Println(IsValid("khaled"))
+	fmt.Println(IsValid("alaa"))
+	fmt.Println(IsValid("khaled-"))
+	fmt.Println(IsValid("a--laa"))
+	fmt.Println(IsValid("khal*ed"))
+}
+
+func IsValid(username string) bool {
 	if strings.HasPrefix(username, "-") ||
 		strings.HasSuffix(username, "-") ||
 		strings.Contains(username, "--") {
-		return
+		return false
 	}
 	if match, _ := regexp.MatchString(`^[a-zA-Z0-9-]{3,39}$`, username); !match {
-		return
+		return false
 	}
-	fmt.Println(username)
+	return true
 }
