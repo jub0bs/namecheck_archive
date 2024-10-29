775381b42abe6ead432224b091ae73c3abc9511b
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-29 10:05:13 +0100

    add IsAvailable function

--- github/github.go
+++ github/github.go
@@ -1,6 +1,9 @@
 package github
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
 	"regexp"
 	"strings"
 )
@@ -12,4 +15,21 @@ func IsValid(username string) bool {
 		!strings.HasSuffix(username, "-") &&
 		!strings.Contains(username, "--") &&
 		re.MatchString(username)
+}
+
+func IsAvailable(username string) (bool, error) {
+	addr := fmt.Sprintf("https://github.com/%s", username)
+	resp, err := http.Get(addr)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	switch resp.StatusCode {
+	case http.StatusNotFound:
+		return true, nil
+	case http.StatusOK:
+		return false, nil
+	default:
+		return false, errors.New("unknown availability")
+	}
 }
