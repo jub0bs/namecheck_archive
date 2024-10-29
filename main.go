5c2166272e827b53145dbc35c2d2d599041d8aa6
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-29 16:54:10 +0100

    declare interface types to reduce code duplication

--- main.go
+++ main.go
@@ -9,36 +9,40 @@ import (
 	"github.com/jub0bs/namecheck/reddit"
 )
 
+type Validator interface {
+	IsValid(string) bool
+}
+
+type Availabler interface {
+	IsAvailable(string) (bool, error)
+}
+
+type Checker interface {
+	Validator
+	Availabler
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "usage: namecheck <username>")
 		os.Exit(1)
 	}
 	username := os.Args[1]
-	gh := github.GitHub{
-		Client: http.DefaultClient,
+	checkers := []Checker{
+		&github.GitHub{Client: http.DefaultClient},
+		&reddit.Reddit{Client: http.DefaultClient},
 	}
-	valid := gh.IsValid(username)
-	fmt.Printf("validity of %q on GitHub: %t\n", username, valid)
-	if valid {
-		avail, err := gh.IsAvailable(username)
+	for _, checker := range checkers {
+		valid := checker.IsValid(username)
+		fmt.Printf("validity of %q on ???: %t\n", username, valid)
+		if !valid {
+			continue
+		}
+		avail, err := checker.IsAvailable(username)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Printf("availability of %q on GitHub: %t\n", username, avail)
-	}
-	rd := reddit.Reddit{
-		Client: http.DefaultClient,
-	}
-	valid = rd.IsValid(username)
-	fmt.Printf("validity of %q on Reddit: %t\n", username, valid)
-	if valid {
-		avail, err := rd.IsAvailable(username)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("availability of %q on Reddit: %t\n", username, avail)
+		fmt.Printf("availability of %q on ???: %t\n", username, avail)
 	}
 }
