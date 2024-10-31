390fef46d312ba6c1a7a689ab0f0571f844b31e3
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-31 10:10:29 +0100

    fail processing at the first error received (... but we may leak goroutines!)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,7 +17,6 @@ type Result struct {
 	Platform  string `json:"platform"`
 	Valid     bool   `json:"valid"`
 	Available bool   `json:"available"`
-	Err       error  `json:"error"`
 }
 
 func main() {
@@ -53,18 +52,30 @@ func handleCheck(w http.ResponseWriter, r *http.Request) {
 		checkers = append(checkers, &gh, &rd)
 	}
 	resultCh := make(chan Result)
+	errorCh := make(chan error)
 	var wg sync.WaitGroup
 	for _, checker := range checkers {
 		wg.Add(1)
-		go check(checker, username, &wg, resultCh)
+		go check(checker, username, &wg, resultCh, errorCh)
 	}
 	go func() {
 		wg.Wait()
 		close(resultCh)
 	}()
 	var results []Result
-	for res := range resultCh {
-		results = append(results, res)
+	var finished bool
+	for !finished {
+		select {
+		case <-errorCh:
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		case res, ok := <-resultCh:
+			if !ok {
+				finished = true
+				continue
+			}
+			results = append(results, res)
+		}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	enc := json.NewEncoder(w)
@@ -78,6 +89,7 @@ func check(
 	username string,
 	wg *sync.WaitGroup,
 	resultCh chan<- Result,
+	errorCh chan<- error,
 ) {
 	defer wg.Done()
 	res := Result{
@@ -89,6 +101,11 @@ func check(
 		resultCh <- res
 		return
 	}
-	res.Available, res.Err = checker.IsAvailable(username)
+	avail, err := checker.IsAvailable(username)
+	if err != nil {
+		errorCh <- err
+		return
+	}
+	res.Available = avail
 	resultCh <- res
 }
