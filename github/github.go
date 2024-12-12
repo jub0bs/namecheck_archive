c0313253594446f8e22f9b39c516256381624a0c
jub0bs <jcretel-infosec+github@protonmail.com>
2024-12-12 16:43:44 +0100

    implement availability check for GitHub

--- github/github.go
+++ github/github.go
@@ -1,6 +1,8 @@
 package github
 
 import (
+	"fmt"
+	"net/http"
 	"regexp"
 	"strings"
 )
@@ -12,4 +14,21 @@ func IsValid(username string) bool {
 		!strings.HasSuffix(username, "-") &&
 		!strings.Contains(username, "--") &&
 		re.MatchString(username)
+}
+
+func IsAvailable(username string) (bool, error) {
+	addr := "https://github.com/" + username
+	res, err := http.Get(addr)
+	if err != nil {
+		return false, err
+	}
+	res.Body.Close()
+	switch res.StatusCode {
+	case http.StatusNotFound:
+		return true, nil
+	case http.StatusOK:
+		return false, nil
+	default:
+		return false, fmt.Errorf("unexpected status code %d", res.StatusCode)
+	}
 }
--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/jub0bs/namecheck/github"
 )
 
 func main() {
 	username := "jub0bs"
-	fmt.Println(github.IsValid(username))
+	if !github.IsValid(username) {
+		return
+	}
+	avail, err := github.IsAvailable(username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !avail {
+		fmt.Printf("%q is valid but unavailable on GitHub.\n", username)
+		return
+	}
+	fmt.Printf("%q is valid and available on GitHub.\n", username)
 }
