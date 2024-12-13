6956028f3c9a6ff30d861c310fe0c13339fa1172
jub0bs <jcretel-infosec+github@protonmail.com>
2024-12-13 10:27:13 +0100

    use table-driven subtests

--- github/github_test.go
+++ github/github_test.go
@@ -23,9 +23,12 @@ func TestIsValid(t *testing.T) {
 		{"all good", "jub0bs", true},
 	}
 	for _, tc := range testCases {
-		got := github.IsValid(tc.username)
-		if got != tc.want {
-			t.Errorf("%s: github.IsValid(%q): got %t; want %t", tc.desc, tc.username, got, tc.want)
+		f := func(t *testing.T) {
+			got := github.IsValid(tc.username)
+			if got != tc.want {
+				t.Errorf("github.IsValid(%q): got %t; want %t", tc.username, got, tc.want)
+			}
 		}
+		t.Run(tc.desc, f)
 	}
 }
