2adeee3d3f02664800321c730f12d81f7605ad85
jub0bs <jcretel-infosec+github@protonmail.com>
2024-12-13 14:43:10 +0100

    use map instead of slice in table-driven subtests

--- github/github_test.go
+++ github/github_test.go
@@ -9,26 +9,25 @@ import (
 
 func TestIsValid(t *testing.T) {
 	type TestCase struct {
-		desc     string
 		username string
 		want     bool
 	}
-	testCases := []TestCase{
-		{"contains two consecutive hyphens", "jub0bs--on-GitHub", false},
-		{"starts with a hyphen", "-jub0bs-on-GitHub", false},
-		{"ends with a hyphen", "jub0bs-on-GitHub-", false},
-		{"too short", "ab", false},
-		{"too long", strings.Repeat("a", 40), false},
-		{"contains invalid chars", "ju^b0bs", false},
-		{"all good", "jub0bs", true},
+	testCases := map[string]TestCase{
+		"contains two consecutive hyphens": {"jub0bs--on-GitHub", false},
+		"starts with a hyphen":             {"-jub0bs-on-GitHub", false},
+		"ends with a hyphen":               {"jub0bs-on-GitHub-", false},
+		"too short":                        {"ab", false},
+		"too long":                         {strings.Repeat("a", 40), false},
+		"contains invalid chars":           {"ju^b0bs", false},
+		"all good":                         {"jub0bs", true},
 	}
-	for _, tc := range testCases {
+	for desc, tc := range testCases {
 		f := func(t *testing.T) {
 			got := github.IsValid(tc.username)
 			if got != tc.want {
 				t.Errorf("github.IsValid(%q): got %t; want %t", tc.username, got, tc.want)
 			}
 		}
-		t.Run(tc.desc, f)
+		t.Run(desc, f)
 	}
 }
