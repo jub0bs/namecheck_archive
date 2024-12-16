1d1a26adce147e1ff0c1eae599760929b592ba6d
jub0bs <jcretel-infosec+github@protonmail.com>
2024-12-16 15:09:08 +0100

    turn CLI tool into server

---
+++ cmd/server/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/jub0bs/namecheck/github"
+)
+
+type Checker interface {
+	IsValid(string) bool
+	IsAvailable(string) (bool, error)
+	String() string
+}
+
+type Result struct {
+	Platform  string
+	Valid     bool
+	Available bool
+	Err       error
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /check", handleCheck)
+	if err := http.ListenAndServe(":8080", mux); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func handleCheck(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	gh := github.GitHub{
+		Client: http.DefaultClient,
+	}
+	const n = 20
+	checkers := make([]Checker, 0, n)
+	for range n {
+		checkers = append(checkers, &gh)
+	}
+	resultCh := make(chan Result)
+	var wg sync.WaitGroup
+	wg.Add(len(checkers))
+	for _, checker := range checkers {
+		go check(checker, username, &wg, resultCh)
+	}
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+	var results []Result
+	for res := range resultCh {
+		results = append(results, res)
+	}
+	fmt.Fprint(w, results)
+}
+
+func check(
+	checker Checker,
+	username string,
+	wg *sync.WaitGroup,
+	resultCh chan<- Result,
+) {
+	defer wg.Done()
+	res := Result{
+		Platform: checker.String(),
+		Valid:    checker.IsValid(username),
+	}
+	if !res.Valid {
+		resultCh <- res
+		return
+	}
+	res.Available, res.Err = checker.IsAvailable(username)
+	resultCh <- res
+}
